69c9c6bd6377403527bc64d71100918945bfd224
NGUYEN NHAT TRUONG <103358975+Ericc1909@users.noreply.github.com>
2023-06-01 00:08:18 +0700

    commit

---
+++ CODE/Code/QL_DatHang_GiaoHang/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_DatHang_GiaoHang
+{
+    public class Connection
+    {
+        public static string conectionstring = @"Data Source=DESKTOP-G8E20U2;Initial Catalog=Ql_DATHANG_BANHANG_HQTCSDL;Integrated Security=True";
+
+    }
+}
